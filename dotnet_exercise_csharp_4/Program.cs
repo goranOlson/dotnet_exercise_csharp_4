537f97d6e0d83b202fca6dbfeb97e91b73390b62
Göran Olson <goran@famolson.se>
2026-06-03 08:41:21 +0200

    Created initial Program class

--- dotnet_exercise_csharp_4/Program.cs
+++ dotnet_exercise_csharp_4/Program.cs
@@ -1,10 +1,38 @@
 ﻿namespace dotnet_exercise_csharp_4
 {
-    internal class Program
+    class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
-            Console.WriteLine("Hello, World!");
+            List<object> devices = new List<object>();
+            // TODO:
+            // Skapa minst fyra objekt:
+            // Washer, Refrigerator, Oven och RobotVacuum.
+            // Lägg till dem i listan devices.
+            RunMorningRoutine(devices);
+            Console.WriteLine();
+            ReportAllEnergy(devices);
+        }
+        static void RunMorningRoutine(List<object> devices)
+        {
+            foreach (object device in devices)
+            {
+                // TODO:
+                // 1. Kontrollera vilken typ device är.
+                // 2. Casta till rätt typ.
+                // 3. Anropa rätt startmetod.
+                // 4. Anropa rätt stoppmetod.
+            }
+        }
+        static void ReportAllEnergy(List<object> devices)
+        {
+            foreach (object device in devices)
+            {
+                // TODO:
+                // 1. Kontrollera vilken typ device är. 
+                // 2. Casta till rätt typ.
+                // 3. Anropa rätt energimetod.
+            }
         }
     }
 }
