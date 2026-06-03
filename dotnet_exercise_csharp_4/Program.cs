e679950d0abcdf158c3698be7b86d9fc26256961
Göran Olson <goran@famolson.se>
2026-06-03 09:29:20 +0200

    Created Washer class

--- dotnet_exercise_csharp_4/Program.cs
+++ dotnet_exercise_csharp_4/Program.cs
@@ -1,6 +1,9 @@
-﻿namespace dotnet_exercise_csharp_4
+﻿using System.Diagnostics;
+using System.Runtime.InteropServices.Swift;
+
+namespace dotnet_exercise_csharp_4
 {
-    class Program
+    partial class Program
     {
         static void Main()
         {
@@ -9,9 +12,15 @@
             // Skapa minst fyra objekt:
             // Washer, Refrigerator, Oven och RobotVacuum.
             // Lägg till dem i listan devices.
+
+            devices.Add(new Washer("Electrolux", 8));
+
+
             RunMorningRoutine(devices);
             Console.WriteLine();
             ReportAllEnergy(devices);
+
+            
         }
         static void RunMorningRoutine(List<object> devices)
         {
@@ -22,6 +31,30 @@
                 // 2. Casta till rätt typ.
                 // 3. Anropa rätt startmetod.
                 // 4. Anropa rätt stoppmetod.
+
+                //Console.WriteLine("GetType() => " + device.GetType());
+                //Console.WriteLine("Name: " + device.GetType().Name);
+                string className = device.GetType().Name;
+                
+                switch (className)
+                {
+                    case "Washer":
+                        Console.WriteLine("=> Washer");
+                        Washer washer = (Washer)device;
+                        washer.StartWash();
+                        washer.StopWash();
+                        break;
+
+                    default:
+                        Console.WriteLine("Classtype not implemented!");
+                        break;
+                }
+
+
+                
+
+
+                
             }
         }
         static void ReportAllEnergy(List<object> devices)
---
+++ dotnet_exercise_csharp_4/Washer.cs
@@ -0,0 +1,39 @@
+﻿namespace dotnet_exercise_csharp_4
+{
+    partial class Program
+    {
+        public class Washer
+        {
+            public string Brand { get; }
+            public int CapacityKg { get; }
+
+            public Washer(string brand, int capacityKg)
+            {
+                Brand = brand;
+                CapacityKg = capacityKg;
+            }
+
+            public void StartWash()
+            {
+                Console.WriteLine("StartWash()");
+            }
+
+            public void StopWash()
+            {
+                Console.WriteLine("StopWash()");
+            }
+
+            public void PrintWashEnergy()
+            {
+                Console.WriteLine("PrintWashEnergy()");
+            }
+
+
+
+            public override string ToString()
+            {
+                return $"{Brand} {CapacityKg} kg";
+            }
+        }
+    }
+}
