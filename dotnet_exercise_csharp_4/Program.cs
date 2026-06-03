db673470c09d5b3ca5efbd801a706873eeadd012
Göran Olson <goran@famolson.se>
2026-06-03 08:38:19 +0200

    Add project files.

---
+++ dotnet_exercise_csharp_4/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace dotnet_exercise_csharp_4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
